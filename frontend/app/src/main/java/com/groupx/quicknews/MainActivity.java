a4bdd179b5adbb9dbcc0b4a0ac5b22695021a5f9
reclayton <70980160+reclayton@users.noreply.github.com>
2023-10-27 14:25:08 -0700

    basic articles view

---
+++ frontend/app/src/main/java/com/groupx/quicknews/MainActivity.java
@@ -0,0 +1,33 @@
+package com.groupx.quicknews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button forumButton  ;
+    private Button articlesButton;
+    final static String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        forumButton = findViewById(R.id.forum_button);
+        articlesButton = findViewById(R.id.article_button);
+
+        articlesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Trying to open google maps");
+                Intent mapsIntent = new Intent(MainActivity.this, ArticlesActivity.class);
+                startActivity(mapsIntent);
+            }
+        });
+    }
+}
