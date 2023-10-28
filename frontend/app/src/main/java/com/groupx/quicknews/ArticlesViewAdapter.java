56ceaa09db9fd4dffc2e5f222b18ef4f90780e72
reclayton <70980160+reclayton@users.noreply.github.com>
2023-10-27 18:12:16 -0700

    added article cardview

--- frontend/app/src/main/java/com/groupx/quicknews/ArticlesViewAdapter.java
+++ frontend/app/src/main/java/com/groupx/quicknews/ArticlesViewAdapter.java
@@ -1,9 +1,14 @@
 package com.groupx.quicknews;
 
 import android.content.Context;
+import android.content.Intent;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,23 +18,44 @@ import java.util.List;
 
 public class ArticlesViewAdapter extends RecyclerView.Adapter<ArticleViewHolder> {
 
+    final static String TAG = "ArticleView";
     public ArticlesViewAdapter(Context context, List<Article> articles) {
         this.context = context;
         this.articles = articles;
     }
 
-    Context context;
-    List<Article> articles;
+    private Context context;
+    private List<Article> articles;
     @NonNull
     @Override
     public ArticleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ArticleViewHolder(LayoutInflater.from(context).inflate(R.layout.article_view, parent, false));
+        ArticleViewHolder holder = new ArticleViewHolder(LayoutInflater.from(context).inflate(R.layout.article_view, parent, false));
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, holder.articleTitle.getText().toString());
+
+                if (holder.hiddenView.getVisibility() == View.VISIBLE) {
+                    // The transition of the hiddenView is carried out by the TransitionManager class.
+                    // Here we use an object of the AutoTransition Class to create a default transition
+                    TransitionManager.beginDelayedTransition(holder.cardView, new AutoTransition());
+                    holder.hiddenView.setVisibility(View.GONE);
+                }
+                // If the CardView is not expanded, set its visibility to
+                // visible and change the expand more icon to expand less.
+                else {
+                    TransitionManager.beginDelayedTransition(holder.cardView, new AutoTransition());
+                    holder.hiddenView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ArticleViewHolder holder, int position) {
         holder.articleTitle.setText(articles.get(position).getTitle());
-
+        holder.articleSynopsis.setText(articles.get(position).getSynopsis());
     }
 
     @Override
