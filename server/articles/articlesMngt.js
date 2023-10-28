55022fea5710a9dc544c6779d11de2514cb524b4
Thong Dinh <minhthongdinh318@gmail.com>
2023-10-28 10:31:08 -0700

    Add init db for article management module - testing purpose

--- server/articles/articlesMngt.js
+++ server/articles/articlesMngt.js
@@ -7,6 +7,41 @@ const defArticle = {
     "publishedDate": null,
 }
 
+// Helper Functions --->
+function createNewArticle(articleId){
+    var newArticle = JSON.parse(JSON.stringify(defArticle))
+    newArticle.articleId = articleId
+    newArticle.views = 10
+    if (articleId % 2 == 0){
+        newArticle.content = "Summarized content of article"
+        newArticle.publisher = "CBC"
+        newArticle.categories = ["sport","criminal"]
+        newArticle.publishedDate = new Date("2023-2-29").toISOString()
+    }
+    else{
+        newArticle.content = "Something about CPEN 321 and News"
+        newArticle.publisher = "CNN"
+        newArticle.categories = ["education", "environment"]
+        newArticle.publishedDate = new Date("2023-10-20").toISOString()
+    }
+    return newArticle
+}
+
+// <-- Helper Functions
+//testing purpose -->
+async function initADb(client, initNum){
+    try {
+        for (var id = 1; id < initNum; id++){
+            var newArticle = createNewArticle(id)
+            await client.db("articledb").collection("articles").insertOne(newArticle)
+        }
+        
+        return("success\n")
+    } catch (error) {
+        return (error)
+    }
+}
+//<-- testing purpose 
 async function searchById(client, articleId){
     try {
         var foundArticle  = await client.db("articledb").collection("articles").find({"articleId": articleId});
@@ -39,4 +74,4 @@ async function searchByFilter(client, query){
     }
 }
 
-export {searchByFilter,searchById}
+export {searchByFilter,searchById, initADb}
