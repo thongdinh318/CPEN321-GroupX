d29dabef5d4b32fe193d1d1c69b3fc0d4ff9c4fd
reclayton <70980160+reclayton@users.noreply.github.com>
2023-10-27 18:12:16 -0700

    added article cardview

--- frontend/app/src/main/java/com/groupx/quicknews/ArticleViewHolder.java
+++ frontend/app/src/main/java/com/groupx/quicknews/ArticleViewHolder.java
@@ -1,15 +1,24 @@
 package com.groupx.quicknews;
 
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ArticleViewHolder extends RecyclerView.ViewHolder {
-    TextView articleTitle;
-    public ArticleViewHolder(@NonNull View itemView) {
-        super(itemView);
+    public TextView articleTitle;
+    public TextView articleSynopsis;
+    public LinearLayout hiddenView;
+    public CardView cardView;
+    public ArticleViewHolder(@NonNull View view) {
+        super(view);
         articleTitle = itemView.findViewById(R.id.article_title);
+        articleSynopsis = itemView.findViewById(R.id.article_synopsis);
+        hiddenView = itemView.findViewById(R.id.hidden_view);
+        cardView = itemView.findViewById(R.id.base_cardview);
     }
+
 }
