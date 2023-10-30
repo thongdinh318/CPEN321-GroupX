67b837eacbfd733237d110173eb7c9223cbbde26
reclayton <70980160+reclayton@users.noreply.github.com>
2023-10-30 14:53:02 -0700

    added login

--- frontend/app/src/main/java/com/groupx/quicknews/ArticlesActivity.java
+++ frontend/app/src/main/java/com/groupx/quicknews/ArticlesActivity.java
@@ -32,13 +32,15 @@ private ActivityArticlesBinding binding;
         //CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
         //toolBarLayout.setTitle(getTitle());
 
-        List<Article> palceholderArticles = new ArrayList<Article>();
-        palceholderArticles.add(new Article("Article1", "https://www.cbc.ca/player/play/2277156419849", "Lorem Ipsum"));
-        palceholderArticles.add(new Article("Article2", "https://www.cbc.ca/news/world/israel-ground-raid-gaza-city-us-strikes-syria-un-vote-eu-members-ceasefire-1.7010037", "Lorem Ipsum"));
+        List<Article> palceHolderArticles = new ArrayList<Article>();
+        palceHolderArticles.add(new Article("Article1", "https://www.cbc.ca/player/play/2277156419849", "Lorem Ipsum"));
+        palceHolderArticles.add(new Article("Article2", "https://www.cbc.ca/news/world/israel-ground-raid-gaza-city-us-strikes-syria-un-vote-eu-members-ceasefire-1.7010037", "Lorem Ipsum"));
 
         articleView = findViewById(R.id.article_view);
         articleView.setLayoutManager(new LinearLayoutManager(this));
-        articleView.setAdapter(new ArticlesViewAdapter(getApplicationContext(), palceholderArticles));
+        articleView.setAdapter(new ArticlesViewAdapter(getApplicationContext(), palceHolderArticles));
 
     }
+    //TODO: get articles from server oncreate
+    //TODO: get recommended articles and create new adapter when selected
 }
---
+++ frontend/app/src/main/java/com/groupx/quicknews/LoginActivity.java
@@ -0,0 +1,104 @@
+package com.groupx.quicknews;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private SignInButton googleSignInButton;
+    private int RC_SIGN_IN = 1;
+    final static String TAG = "LoginActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        googleSignInButton = findViewById(R.id.sign_in_button);
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+
+            //AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+            //builder.setMessage("Error occurred while Signing in. Please try again").setTitle("Sign In Failed");
+            //AlertDialog dialog = builder.create();
+
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.d(TAG, "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        //TODO: pass ID to backend for auth, display alert if not signed in?
+        if (account == null) {
+            Log.d(TAG, "No user signed in");
+        }
+        else {
+            //send token to back end
+            Intent signInIntent = new Intent(LoginActivity.this, MainActivity.class);
+            //SignInIntent.putExtra("user", account.getGivenName() + " " + account.getFamilyName());
+            startActivity(signInIntent);
+        }
+    }
+}
