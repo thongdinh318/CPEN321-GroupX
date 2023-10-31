f757f72505a0275f7358c4ab75143e0998686dbb
reclayton <70980160+reclayton@users.noreply.github.com>
2023-10-30 22:46:22 -0700

    fixed forum post button

--- frontend/app/src/main/java/com/groupx/quicknews/ForumActivity.java
+++ frontend/app/src/main/java/com/groupx/quicknews/ForumActivity.java
@@ -1,6 +1,8 @@
 package com.groupx.quicknews;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -16,6 +18,7 @@ public class ForumActivity extends AppCompatActivity {
 
     private ActivityForumBinding binding;
     private RecyclerView forumView;
+    private Button postButton;
 
     //TODO: pass in forum details so correct endpoint is used when adding new messages
     @Override
@@ -31,26 +34,18 @@ public class ForumActivity extends AppCompatActivity {
         List<Comment> placeHolderComments = new ArrayList<Comment>();
         placeHolderComments.add(new Comment("User1", "You're opinions are terrible and you should feel bad about them"));
         placeHolderComments.add(new Comment("User2", "*your. at least learn to spell"));
-        placeHolderComments.add(new Comment("User1", "You're opinions are terrible and you should feel bad about them"));
-        placeHolderComments.add(new Comment("User2", "*your. at least learn to spell"));
-        placeHolderComments.add(new Comment("User1", "You're opinions are terrible and you should feel bad about them"));
-        placeHolderComments.add(new Comment("User2", "*your. at least learn to spell"));
-        placeHolderComments.add(new Comment("User1", "You're opinions are terrible and you should feel bad about them"));
-        placeHolderComments.add(new Comment("User2", "*your. at least learn to spell"));
-        placeHolderComments.add(new Comment("User1", "You're opinions are terrible and you should feel bad about them"));
-        placeHolderComments.add(new Comment("User2", "*your. at least learn to spell"));
-        placeHolderComments.add(new Comment("User1", "You're opinions are terrible and you should feel bad about them"));
-        placeHolderComments.add(new Comment("User2", "*your. at least learn to spell"));
-        placeHolderComments.add(new Comment("User1", "You're opinions are terrible and you should feel bad about them"));
-        placeHolderComments.add(new Comment("User2", "*your. at least learn to spell"));
-        placeHolderComments.add(new Comment("User1", "You're opinions are terrible and you should feel bad about them"));
-        placeHolderComments.add(new Comment("User2", "*your. at least learn to spell"));
-        placeHolderComments.add(new Comment("User1", "You're opinions are terrible and you should feel bad about them"));
-        placeHolderComments.add(new Comment("User2", "*your. at least learn to spell"));
 
         forumView = findViewById(R.id.comment_view);
         forumView.setLayoutManager(new LinearLayoutManager(this));
         forumView.setAdapter(new CommentsViewAdapter(getApplicationContext(), placeHolderComments));
+
+        postButton = findViewById(R.id.button_post);
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO:manage posting comments to server
+            }
+        });
     }
 
     //TODO: get comments from server, endpoint details need to be passed in
