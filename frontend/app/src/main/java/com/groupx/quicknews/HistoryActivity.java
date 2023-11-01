63e970cf7eae90c464a99067ef80c8a6efcaf344
Thong Dinh <minhthongdinh318@gmail.com>
2023-11-01 06:27:39 -0700

    Add making request to the server

--- frontend/app/src/main/java/com/groupx/quicknews/HistoryActivity.java
+++ frontend/app/src/main/java/com/groupx/quicknews/HistoryActivity.java
@@ -4,19 +4,58 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.groupx.quicknews.helpers.HttpClient;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.Response;
+
 public class HistoryActivity extends AppCompatActivity {
 
+    private String userId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
-        int userId;
+
         Bundle extras = getIntent().getExtras();
 
         if (extras != null){
-            userId = extras.getInt("USER_ID");
+            userId = extras.getString("USER_ID");
         }
         // Make a get request to get the history of this user
+        String getUrl = getString(R.string.server_client_id) +"profile/"+ userId +"/history";
+        HashMap<Integer,String> readingList = new HashMap<>();
+        HttpClient.getRequest(getUrl, new HttpClient.ApiCallback() {
+            @Override
+            public void onResponse(Response response) {
+                try {
+                    if (response.code() == 200) {
+                        JSONArray userHistory = new JSONArray(response.body().toString());
+                        if (userHistory.length() > 0) {
+                            for (int i = 0; i < userHistory.length(); i++) {
+                                JSONObject article = userHistory.getJSONObject(i);
+                                readingList.put(article.getInt("articleId"), article.getString("title"));
+                            }
+                        }
+                    }
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            @Override
+            public void onFailure(Exception e) {
+            }
+        });
+
+        //Display the list of article for the user here
     }
 }
