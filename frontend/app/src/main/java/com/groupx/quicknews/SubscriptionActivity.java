bef81dcad04d83da3ae09e7c7010e9f9fcda3c9c
reclayton <70980160+reclayton@users.noreply.github.com>
2023-11-02 23:48:54 -0700

    subscriptions working and cleaned up

--- frontend/app/src/main/java/com/groupx/quicknews/SubscriptionActivity.java
+++ frontend/app/src/main/java/com/groupx/quicknews/SubscriptionActivity.java
@@ -1,6 +1,7 @@
 package com.groupx.quicknews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.widget.Switch;
 import android.widget.TextView;
 
 import com.groupx.quicknews.helpers.HttpClient;
+import com.groupx.quicknews.ui.forumlist.ForumsViewAdapter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,7 +28,7 @@ import okhttp3.Response;
 public class SubscriptionActivity extends AppCompatActivity {
 
     final static String TAG = "SubscriptionActivity";
-    private List<String> subscriptionList = new ArrayList<>();
+    private List<String> subscriptionList;
     private String userId = LoginActivity.getUserId();
     private TextView cbc, cnn;
     private Switch cbc_sub, cnn_sub;
@@ -35,30 +37,8 @@ public class SubscriptionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subscription);
+        subscriptionList = new ArrayList<>();
 
-
-        //GET REQUEST GET THE SUB LIST
-        String getUrl = getString(R.string.server_dns) +"profile/"+ userId+"/subscriptions";
-        HttpClient.getRequest(getUrl, new HttpClient.ApiCallback() {
-            @Override
-            public void onResponse(Response response) {
-                try {
-                    if (response.code() == 200) {
-                        JSONArray userList = new JSONArray(response.body().string());
-                        if (userList.length() > 0) {
-                            for (int i = 0; i < userList.length(); i++) {
-                                subscriptionList.add(userList.getString(i));
-                            }
-                        }
-                    }
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            @Override
-            public void onFailure(Exception e) {
-            }
-        });
         cbc = findViewById(R.id.news_publisher_1);
         cbc.setText("CBC");
         cnn = findViewById(R.id.news_publisher_2);
@@ -68,13 +48,7 @@ public class SubscriptionActivity extends AppCompatActivity {
         cbc_sub.setChecked(false);
         cnn_sub = findViewById(R.id.sub_button_2);
         cnn_sub.setChecked(false);
-        for (int i = 0; i < subscriptionList.size(); ++i){
-            if (subscriptionList.get(i).contains("cbc")){
-                cbc_sub.setChecked(true);
-            } else if (subscriptionList.get(i).contains("cnn")) {
-                cnn_sub.setChecked(true);
-            }
-        }
+
         cbc_sub.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
@@ -103,43 +77,96 @@ public class SubscriptionActivity extends AppCompatActivity {
             }
         });
 
+
+
         confirm = findViewById(R.id.sub_confirm_button);
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                JSONArray newSubArr = new JSONArray();
-
-                for (int i = 0; i<subscriptionList.size(); ++i){
-                    newSubArr.put(subscriptionList.get(i));
-                }
-
-                String putUrl = getString(R.string.server_dns)+"profile/"+userId;
-                try {
-                    JSONObject json = new JSONObject();
-                    json.put("subscriptionList", newSubArr);
-                    HttpClient.putRequest(putUrl, json.toString(), new HttpClient.ApiCallback() {
-                        @Override
-                        public void onResponse(Response response) {
-                            if (response.code() == 200){
-                                String result = "true";//response.body().string();
-                                if (result == "true"){
-                                    //TODO: Switch views here
-                                    Log.d(TAG,result);
-                                    //Return to previous view
-                                    Intent profileIntent = new Intent(SubscriptionActivity.this, ProfileActivity.class);
-                                    startActivity(profileIntent);
-                                }
-                            }
-                        }
-                        @Override
-                        public void onFailure(Exception e) {
-                            Log.d(TAG,e.getMessage());
-                        }
-                    });
-                } catch (JSONException e) {
-                    throw new RuntimeException(e);
-                }
+                updateSubscriptionList();
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        subscriptionList.clear();
+        getSubscriptionList ();
+    }
+
+    private void getSubscriptionList () {
+        //GET REQUEST GET THE SUB LIST
+        String getUrl = getString(R.string.server_dns) +"profile/"+ userId+"/subscriptions";
+        HttpClient.getRequest(getUrl, new HttpClient.ApiCallback() {
+            @Override
+            public void onResponse(Response response) {
+                try {
+                    if (response.code() == 200) {
+                        String responseBody = response.body().string();
+                        JSONArray userList = new JSONArray(responseBody);
+                        Log.d(TAG, responseBody);
+                        if (userList.length() > 0) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    for (int i = 0; i < userList.length(); i++) {
+                                        String sub;
+                                        try {
+                                            sub = userList.getString(i);
+                                        } catch (JSONException e) {
+                                            throw new RuntimeException(e);
+                                        }
+                                        if (sub.contains("cbc")){
+                                            cbc_sub.setChecked(true);
+                                        } else if (sub.contains("cnn")) {
+                                            cnn_sub.setChecked(true);
+                                        }
+                                    }
+                                }
+                            });
+
+                        }
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            @Override
+            public void onFailure(Exception e) {
+            }
+        });
+    }
+
+    private void updateSubscriptionList () {
+            JSONArray newSubArr = new JSONArray();
+
+            for (int i = 0; i<subscriptionList.size(); ++i){
+                newSubArr.put(subscriptionList.get(i));
+            }
+
+            String putUrl = getString(R.string.server_dns)+"profile/"+userId;
+            try {
+                JSONObject json = new JSONObject();
+                json.put("subscriptionList", newSubArr);
+                HttpClient.putRequest(putUrl, json.toString(), new HttpClient.ApiCallback() {
+                    @Override
+                    public void onResponse(Response response) {
+                        if (response.code() == 200){
+                            String result = "true";//response.body().string();
+                            if (result == "true"){
+                                //TODO: Return to original view here
+                                Log.d(TAG,result);
+                            }
+                        }
+                    }
+                    @Override
+                    public void onFailure(Exception e) {
+                        Log.d(TAG,e.getMessage());
+                    }
+                });
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
 }
