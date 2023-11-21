e52f5cade6bf8c82f2ff84c0dc0d736aa5fd8ee5
reclayton <70980160+reclayton@users.noreply.github.com>
2023-11-21 01:46:36 -0800

    added bottom navbar

--- frontend/app/src/main/java/com/groupx/quicknews/ForumsListActivity.java
+++ frontend/app/src/main/java/com/groupx/quicknews/ForumsListActivity.java
@@ -1,15 +1,20 @@
 package com.groupx.quicknews;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.groupx.quicknews.databinding.ActivityForumsListBinding;
+import com.groupx.quicknews.databinding.ActivityMainBinding;
 import com.groupx.quicknews.helpers.HttpClient;
 import com.groupx.quicknews.ui.forumlist.Forum;
 import com.groupx.quicknews.ui.forumlist.ForumsViewAdapter;
@@ -26,7 +31,7 @@ public class ForumsListActivity extends AppCompatActivity {
     private RecyclerView viewForum;
     private List<Forum> forums;
     final static String TAG = "ForumsListActivity";
-    
+
     // ChatGPT usage: No.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +45,25 @@ public class ForumsListActivity extends AppCompatActivity {
 
         Toolbar toolbar = binding.toolbar;
         setSupportActionBar(toolbar);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setSelectedItemId(R.id.action_forums);
+        bottomNavigationView.findViewById(R.id.action_forums).setEnabled(false);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemID = item.getItemId();
+            if (itemID == R.id.action_home) {
+                Log.d(TAG, "Trying to open main view");
+                Intent mainIntent = new Intent(ForumsListActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                overridePendingTransition(0, 0);
+                return true;
+            } else if (itemID == R.id.action_forums) {
+                return true;
+            }
+            return false;
+        });
 
         viewForum = findViewById(R.id.view_forum);
         getForums();
--- frontend/app/src/main/java/com/groupx/quicknews/MainActivity.java
+++ frontend/app/src/main/java/com/groupx/quicknews/MainActivity.java
@@ -1,5 +1,6 @@
 package com.groupx.quicknews;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
@@ -25,13 +26,13 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.groupx.quicknews.databinding.ActivityMainBinding;
 import com.groupx.quicknews.helpers.HttpClient;
 import com.groupx.quicknews.ui.articles.Article;
-import com.groupx.quicknews.ui.forum.Comment;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -42,6 +43,7 @@ import java.util.List;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     private EditText category;
     private Button fromButton;
     private Button toButton;
@@ -60,7 +62,6 @@ public class MainActivity extends AppCompatActivity {
         Button filterSearchButton;
         SearchView searchView;
         Button recommendedArticlesButton;
-        Button forumButton;
 
         initDatePicker(DATEPICKER_FROM);
         initDatePicker(DATEPICKER_TO);
@@ -77,6 +78,27 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, items);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         publisher.setAdapter(adapter);
+
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+        bottomNavigationView.findViewById(R.id.action_home).setEnabled(false);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemID = item.getItemId();
+            if (itemID == R.id.action_home) {
+                return true;
+            }
+            else if (itemID == R.id.action_forums) {
+                Log.d(TAG, "Trying to open forum view");
+                Intent forumIntent = new Intent(MainActivity.this, ForumsListActivity.class);
+                startActivity(forumIntent);
+                overridePendingTransition(0, 0);
+                return true;
+            }
+            return false;
+        });
+
 
         fromButton.setOnClickListener(new View.OnClickListener() {
             // ChatGPT usage: No.
@@ -124,65 +146,9 @@ public class MainActivity extends AppCompatActivity {
                 query += "&before="+toDate.replace(" ", "-");
                 query+= "&after="+fromDate.replace(" ", "-");
                 String url = getString(R.string.server_dns) + "article/filter/search?"+query;
-                getRequestAndSwitchViews(url, ArticlesActivity.class);
-
-
-//                HttpClient.getRequest(url, new HttpClient.ApiCallback() {
-//                    // ChatGPT usage: No.
-//                    @Override
-//                    public void onResponse(Response response) {
-//                        String json = null;
-//                        try {
-//                            //Check if error occurred on the server
-//                            if (response.code() == 400){
-//                                String msg = response.body().string();
-//                                runOnUiThread(new Runnable() {
-//                                    // ChatGPT usage: No.
-//                                    public void run() {
-//                                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
-//                                    }
-//                                });
-//                            }
-//                            else {
-//                                json = response.body().string();
-////                            Log.d(TAG, json);
-//                                JSONArray res = new JSONArray(json);
-//                                //Check for any matched articles, length == 0 means no match
-//                                if (res.length() == 0) {
-//                                    runOnUiThread(new Runnable() {
-//                                        // ChatGPT usage: No.
-//                                        public void run() {
-//                                            Toast.makeText(MainActivity.this, "No Articles Found", Toast.LENGTH_LONG).show();
-//                                        }
-//                                    });
-//                                } else {
-//                                    articleList = new ArrayList<Article>(); //clear the articleList to refresh ArticleActivity when switching
-//                                    for (int i = 0; i < res.length(); i++) {
-//                                        JSONObject articleJson = res.getJSONObject(i);
-//                                        Article article = new Article(articleJson.getString("title"),
-//                                                articleJson.getString("url"),
-//                                                articleJson.getString("content"),
-//                                                articleJson.getInt("articleId"));
-//                                        articleList.add(article);
-//                                    }
-//                                    Intent articleIntent = new Intent(MainActivity.this, ArticlesActivity.class);
-//                                    startActivity(articleIntent);
-//                                }
-//                            }
-//                        } catch (IOException | JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Exception e) {
-//                        e.printStackTrace();
-//                    }
-//                });
+                getArticlesAndSwitchViews(url, ArticlesActivity.class);
             }
         });
-        forumButton = findViewById(R.id.forum_button);
         recommendedArticlesButton = findViewById(R.id.article_button);
         searchView = findViewById(R.id.searchView);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -191,62 +157,7 @@ public class MainActivity extends AppCompatActivity {
             public boolean onQueryTextSubmit(String query) {
                 String url = getString(R.string.server_dns) + "article/kwsearch/search?keyWord="+query;
                 Log.d(TAG,url);
-                getRequestAndSwitchViews(url, ArticlesActivity.class);
-//                HttpClient.getRequest(url, new HttpClient.ApiCallback() {
-//                    // ChatGPT usage: No.
-//                    @Override
-//                    public void onResponse(Response response) {
-//                        try {
-//                            //Status code check
-//                            if (response.code() == 400){
-//                                String msg = response.body().string();
-//                                runOnUiThread(new Runnable() {
-//                                    // ChatGPT usage: No.
-//                                    public void run() {
-//                                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
-//                                    }
-//                                });
-//
-//                            }
-//                            else{
-//                                String json = response.body().string();
-//                                Log.d(TAG, json);
-//                                JSONArray res = new JSONArray(json);
-//                                //Check if matched articles found
-//                                if (res.length() == 0){
-//                                    runOnUiThread(new Runnable() {
-//                                        // ChatGPT usage: No.
-//                                        public void run() {
-//                                            Toast.makeText(MainActivity.this, "No Articles Found", Toast.LENGTH_LONG).show();
-//                                        }
-//                                    });
-//
-//                                }
-//                                else{
-//                                    articleList = new ArrayList<Article>();
-//                                    for (int i = 0; i < res.length(); i++){
-//                                        JSONObject articleJson = res.getJSONObject(i);
-//                                        Article article = new Article(articleJson.getString("title"),
-//                                                articleJson.getString("url"),
-//                                                articleJson.getString("content"),
-//                                                articleJson.getInt("articleId"));
-//                                        articleList.add(article);
-//                                    }
-//                                    Intent articleIntent = new Intent(MainActivity.this, ArticlesActivity.class);;
-//                                    startActivity(articleIntent);
-//                                }
-//                            }
-//                        } catch (JSONException | IOException e) {
-//                            throw new RuntimeException(e);
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Exception e) {
-//                        e.printStackTrace();
-//                    }
-//                });
-
+                getArticlesAndSwitchViews(url, ArticlesActivity.class);
                 return true;
             }
 
@@ -263,78 +174,12 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "Trying to open articles view");
                 String userId = LoginActivity.getUserId();
                 String url = getString(R.string.server_dns) + "recommend/article/"+userId;
-                getRequestAndSwitchViews(url, ArticlesActivity.class);
-//                HttpClient.getRequest(url, new HttpClient.ApiCallback() {
-//                    // ChatGPT usage: No.
-//                    @Override
-//                    public void onResponse(Response response) {
-//                        String json = null;
-//                        try {
-//                            Log.d(TAG, String.valueOf(response.code()));
-//                            // Status code check
-//                            if (response.code() == 400){
-//                                String msg = response.body().string();
-//                                runOnUiThread(new Runnable() {
-//                                    public void run() {
-//                                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
-//                                    }
-//                                });
-//
-//                            }
-//                            else {
-//                                json = response.body().string();
-//                                Log.d(TAG,json);
-//                                JSONArray res = new JSONArray(json);
-//                                //Matched articles check
-//                                if (res.length() == 0){
-//                                    runOnUiThread(new Runnable() {
-//                                        public void run() {
-//                                            Toast.makeText(MainActivity.this, "No Recommendation", Toast.LENGTH_LONG).show();
-//                                        }
-//                                    });
-//                                }
-//                                else {
-//                                    articleList = new ArrayList<Article>();
-//                                    for (int i = 0; i < res.length(); i++) {
-//                                        JSONObject articleJson = res.getJSONObject(i);
-//                                        Article article = new Article(articleJson.getString("title"),
-//                                                articleJson.getString("url"),
-//                                                articleJson.getString("content"),
-//                                                articleJson.getInt("articleId"));
-//                                        articleList.add(article);
-//                                    }
-//                                Intent articleIntent = new Intent(MainActivity.this, ArticlesActivity.class);;
-//                                startActivity(articleIntent);
-//                                }
-//                            }
-//
-//                        } catch (IOException | JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Exception e) {
-//                        e.printStackTrace();
-//                    }
-//                });
-
-            }
-        });
-
-        forumButton.setOnClickListener( new View.OnClickListener() {
-            // ChatGPT usage: No.
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "Trying to open forum view");
-                Intent forumIntent = new Intent(MainActivity.this, ForumsListActivity.class);
-                startActivity(forumIntent);
+                getArticlesAndSwitchViews(url, ArticlesActivity.class);
             }
         });
     }
 
-
-    private void getRequestAndSwitchViews(String url,  Class<?> targetActivity){
+    private void getArticlesAndSwitchViews(String url, Class<?> targetActivity){
         HttpClient.getRequest(url, new HttpClient.ApiCallback() {
             // ChatGPT usage: No.
             @Override
