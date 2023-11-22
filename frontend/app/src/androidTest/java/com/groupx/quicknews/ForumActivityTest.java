af5ca47a77c843f4386d900383ed7226166e5aa6
reclayton <70980160+reclayton@users.noreply.github.com>
2023-11-22 00:34:50 -0800

    separated test utility

--- frontend/app/src/androidTest/java/com/groupx/quicknews/ForumActivityTest.java
+++ frontend/app/src/androidTest/java/com/groupx/quicknews/ForumActivityTest.java
@@ -13,6 +13,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.isNotEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -22,16 +23,28 @@ import static org.hamcrest.Matchers.is;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.view.ViewTreeObserver;
 
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.IdlingResource;
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.matcher.BoundedMatcher;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+
+import com.groupx.quicknews.util.RecyclerViewIdlingResource;
+import com.groupx.quicknews.util.RecyclerViewMatchers;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 import org.hamcrest.core.IsInstanceOf;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -41,11 +54,12 @@ import org.junit.runner.RunWith;
 public class ForumActivityTest {
 
     @Rule
-    public ActivityScenarioRule<LoginActivity> mActivityScenarioRule =
-            new ActivityScenarioRule<>(LoginActivity.class);
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void forumActivityTest() {
+    public void openForumListTest() {
+
         ViewInteraction bottomNavigationItemView = onView(
                 allOf(withId(R.id.action_forums), withContentDescription("Forums"),
                         childAtPosition(
@@ -56,95 +70,36 @@ public class ForumActivityTest {
                         isDisplayed()));
         bottomNavigationItemView.perform(click());
 
-        ViewInteraction linearLayout = onView(
-                allOf(withParent(withParent(withId(R.id.view_forum))),
-                        isDisplayed()));
-        linearLayout.check(matches(isDisplayed()));
+        ViewInteraction recyclerView = onView(withId(R.id.view_forum));
+        recyclerView.check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE))); // Check if the RecyclerView is visible
+        }
 
-        ViewInteraction recyclerView = onView(
-                allOf(withId(R.id.view_forum),
-                        childAtPosition(
-                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-                                1)));
+    @Rule
+    public ActivityScenarioRule<ForumsListActivity> mForumsActivityRule =
+            new ActivityScenarioRule<>(ForumsListActivity.class);
+    private RecyclerViewIdlingResource idlingResource;
+    @Before
+    public void setUp() {
+        // Register the RecyclerViewIdlingResource before the test
+        final RecyclerView[] recyclerView = new RecyclerView[1];
+        mActivityScenarioRule.getScenario().onActivity(activity -> {
+            recyclerView[0] = activity.findViewById(R.id.view_forum);
+        });
+        idlingResource = new RecyclerViewIdlingResource(recyclerView[0]);
+        IdlingRegistry.getInstance().register(idlingResource);
+    }
+    @Test
+    public void forumListLoadedTest() {
+        ViewInteraction recyclerView = onView(withId(R.id.view_forum));
+        recyclerView.check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        recyclerView.check(matches(RecyclerViewMatchers.withItemCountGreaterThan(0)));
+
         recyclerView.perform(actionOnItemAtPosition(0, click()));
-
-        ViewInteraction editText = onView(
-                allOf(withId(R.id.edit_post), withText("Enter Message"),
-                        withParent(allOf(withId(R.id.layout_make_post),
-                                withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class)))),
-                        isDisplayed()));
-        editText.check(matches(withText("Enter Message")));
-
-        ViewInteraction button = onView(
-                allOf(withId(R.id.button_post), withText("POST"),
-                        withParent(allOf(withId(R.id.layout_make_post),
-                                withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class)))),
-                        isDisplayed()));
-        button.check(matches(isDisplayed()));
-        button.check(matches(isNotEnabled()));
-
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.edit_post),
-                        childAtPosition(
-                                allOf(withId(R.id.layout_make_post),
-                                        childAtPosition(
-                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-                                                2)),
-                                0),
-                        isDisplayed()));
-        appCompatEditText.perform(replaceText("Post 1"), closeSoftKeyboard());
-
-        ViewInteraction materialButton = onView(
-                allOf(withId(R.id.button_post), withText("Post"),
-                        childAtPosition(
-                                allOf(withId(R.id.layout_make_post),
-                                        childAtPosition(
-                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-                                                2)),
-                                1),
-                        isDisplayed()));
-        button.check(matches(isEnabled()));
-        materialButton.perform(click());
-
-        ViewInteraction relativeLayout = onView(
-                allOf(withParent(allOf(withId(R.id.view_comment),
-                                withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class)))),
-                        isDisplayed()));
-        relativeLayout.check(matches(isDisplayed()));
-
-        editText.check(matches(withText("Enter Message")));
-
-        button.check(matches(isNotEnabled()));
-
-        appCompatEditText.perform(replaceText("Post 2"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText4 = onView(
-                allOf(withId(R.id.edit_post),
-                        childAtPosition(
-                                allOf(withId(R.id.layout_make_post),
-                                        childAtPosition(
-                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-                                                2)),
-                                0),
-                        isDisplayed()));
-        appCompatEditText4.perform(replaceText("11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111long11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111long11111111111111111111111111111111111111111111111111long11111111111111111111111111111111111111111111"), closeSoftKeyboard());
-
-        ViewInteraction materialButton4 = onView(
-                allOf(withId(R.id.button_post), withText("Post"),
-                        childAtPosition(
-                                allOf(withId(R.id.layout_make_post),
-                                        childAtPosition(
-                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-                                                2)),
-                                1),
-                        isDisplayed()));
-        materialButton4.perform(click());
-
-        ViewInteraction textView = onView(
-                allOf(withId(R.id.text_comment), withText("11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111long11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111long11111111111111111111111111111111111111111111111111long11111111111111111111111111111111111111111111"),
-                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.RelativeLayout.class))),
-                        isDisplayed()));
-        textView.check(matches(withText("11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111long11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111long11111111111111111111111111111111111111111111111111long11111111111111111111111111111111111111111111")));
+    }
+    @After
+    public void tearDown() {
+        // Unregister the RecyclerViewIdlingResource after the test
+        IdlingRegistry.getInstance().unregister(idlingResource);
     }
 
     private static Matcher<View> childAtPosition(
---
+++ frontend/app/src/androidTest/java/com/groupx/quicknews/util/RecyclerViewMatchers.java
@@ -0,0 +1,24 @@
+package com.groupx.quicknews.util;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.matcher.BoundedMatcher;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
+public class RecyclerViewMatchers {
+    public static Matcher<Object> withItemCountGreaterThan(final int count) {
+        return new BoundedMatcher<Object, RecyclerView>(RecyclerView.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with item count greater than: " + count);
+            }
+
+            @Override
+            protected boolean matchesSafely(RecyclerView recyclerView) {
+                RecyclerView.Adapter adapter = recyclerView.getAdapter();
+                return adapter != null && adapter.getItemCount() > count;
+            }
+        };
+    }
+}
